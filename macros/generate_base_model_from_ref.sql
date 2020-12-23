4268fc9b3b0db802f3ad9d933ee0e5e6e8afe527
OhadSrur <ohadsrur@gmail.com>
2020-12-24 10:48:58 +1100

    Create generate_base_model_from_ref.sql
    Create generate_base_model_from_ref for creating a model from seed.

---
+++ macros/generate_base_model_from_ref.sql
@@ -0,0 +1,34 @@
+{% macro generate_base_model_from_ref(ref_name,include_field_as) %}
+
+{%- set columns = adapter.get_columns_in_relation(ref(ref_name)) -%}
+{% set column_names=columns | map(attribute='name') %}
+{% set base_model_sql %}
+with source as (
+
+    select * from {% raw %}{{ ref({% endraw %}'{{ ref_name }}'{% raw %}) }}{% endraw %}
+
+),
+
+renamed as (
+
+    select
+        {%- for column in column_names %}
+        {{ column }}
+        {%- if include_field_as %} as {{ column | lower }}          
+        {%- endif %}{{"," if not loop.last}}
+        {%- endfor %}
+
+    from source
+
+)
+
+select * from renamed
+{% endset %}
+
+{% if execute %}
+
+{{ log(base_model_sql, info=True) }}
+{% do return(base_model_sql) %}
+
+{% endif %}
+{% endmacro %}
